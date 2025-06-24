f611303b4435aafa3c666a6bc396a7946fa96d43
BravestCheetah <luckedev888@outlook.com>
2025-06-24 18:15:27 +0200

    Started on backend, not full commit, just committing to be able to pull

--- backend/server/server_data.py
+++ backend/server/server_data.py
@@ -1,8 +1,27 @@
+import requests as req
+from misc.errors import RequestJsonFailedError
+
+
+
+class _server_downloads():
+    def __init__(self):
+        pass
+
+    def get_json(self, url):
+        resp = req.get(url)
+
+        if resp.ok: return resp.json()
+        else: raise RequestJsonFailedError(f"An Error Occured When Getting JSON From Url {url}")
+
+    def vanilla(self):
+        pass
+
 
 
 class data():
     def __init__(self) -> None:
-        pass
+        self.downloads = _server_downloads()
 
     def get_jar_download(software, ver) -> None:
         pass
+
