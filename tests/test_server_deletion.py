bee54fbd9e382269d4f9674c00b231553ee4b8fb
BravestCheetah <luckedev888@outlook.com>
2025-06-25 11:37:15 +0200

    Added server deletion test

---
+++ tests/test_server_deletion.py
@@ -0,0 +1,18 @@
+import hashlib
+import pathlib
+
+
+def test_server_creation():
+    from mcserver.backend.server import Server
+    from mcserver.backend.data import rm_server
+
+
+    software = "paper"
+    version = "1.18"
+
+    server = Server(name="test2", motd="testing server", software=software, version=version)
+    server.install_server()
+
+    rm_server("test2", confirm=True)
+
+    assert pathlib.Path.exists(server.path / "test2")
