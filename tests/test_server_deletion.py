27a8c8f86ff2a32f335a7ed4c9a1e519ded4b073
BravestCheetah <luckedev888@outlook.com>
2025-06-25 17:05:09 +0200

    Fixed deletion test failing if creation succeeded last time but deletion failed so this time triggering server already exists error by always deleting the server if it already exists

--- tests/test_server_deletion.py
+++ tests/test_server_deletion.py
@@ -4,13 +4,16 @@ import pathlib
 
 def test_server_creation():
     from mcserver.backend.server_manager import delete_server, create_server
+    from mcserver.backend.data import get_servers, rm_server
 
+    if "deletiontest" in get_servers():
+        rm_server("deletiontest", confirm=True)
 
     software = "paper"
     version = "1.18"
 
-    server = create_server("deletionTest", "hi", software, version)
+    server = create_server("deletiontest", "hi", software, version)
     
-    delete_server("deletionTest")
+    delete_server("deletiontest")
 
     assert pathlib.Path.exists(server.path)
