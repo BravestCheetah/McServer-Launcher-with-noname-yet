f4f668f309be7b8b20969251f433285b3658b216
BravestCheetah <bravestcheetah@gmail.com>
2025-07-21 19:36:45 +0200

    more whitespace :O

--- src/mcserver/webui/index.py
+++ src/mcserver/webui/index.py
@@ -3,14 +3,18 @@ from nicegui import ui
 
 def render(debug: bool = False):
 
+
     ui.query("body").style("font-size: 120%")
     
+
     with ui.tabs().classes("w-full") as top_tabs:
 
         home_top_tab = ui.tab("Home")
         server_top_tab = ui.tab("Server")
     
+
     with ui.tab_panels(top_tabs, value=server_top_tab).classes("w-full"):
+
 
         with ui.tab_panel(home_top_tab):
             ui.separator()
