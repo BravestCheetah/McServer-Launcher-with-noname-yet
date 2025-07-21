b80cbbf0a4e7d521ef032c015b584ae026f9be90
BravestCheetah <bravestcheetah@gmail.com>
2025-07-21 19:34:36 +0200

    further workings on the gui

--- src/mcserver/webui/index.py
+++ src/mcserver/webui/index.py
@@ -2,12 +2,13 @@ from nicegui import ui
 
 
 def render(debug: bool = False):
+
+    ui.query("body").style("font-size: 120%")
     
     with ui.tabs().classes("w-full") as top_tabs:
 
         home_top_tab = ui.tab("Home")
         server_top_tab = ui.tab("Server")
-
     
     with ui.tab_panels(top_tabs, value=server_top_tab).classes("w-full"):
 
@@ -17,16 +18,17 @@ def render(debug: bool = False):
             ui.label("home tab")
 
         
-        with ui.tab_panel(server_top_tab):
+        with ui.tab_panel(server_top_tab).classes('w-full'):
+
+
             ui.separator()
 
 
-            with ui.row().classes('w-half items-center justify-center'):
+            with ui.row().classes('w-full'):
 
-                ui.label("Server Name")
+                server_name_label = ui.label("Server Name").style("font-size: 120%")
                 ui.space()
-                ui.button("Stop", color="red")
-
+                ui.button("Stop", color="red", on_click=lambda: ui.notify('No gui is implemented yet :(', close_button='OK'))
 
 
     ui.run()
