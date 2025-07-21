d9d28f14c7674417887f442fd2846d870a216e78
BravestCheetah <bravestcheetah@gmail.com>
2025-07-21 16:40:18 +0200

    Fixed server_manager commands throwing an error when there are no servers when checking if the server already / doesnt exist

--- src/mcserver/backend/server_manager.py
+++ src/mcserver/backend/server_manager.py
@@ -3,9 +3,11 @@ from mcserver.errors import ServerAlreadyExistsError, ServerDoesNotExistError
 from mcserver.backend.data import get_servers, load_server_data, rm_server
 
 def create_server(name, motd, software, version) -> Server:
-    if name in get_servers():
-        raise ServerAlreadyExistsError
-        return
+    servers = get_servers()
+    if servers:
+        if name in servers:
+            raise ServerAlreadyExistsError
+            return
 
     server = Server(software, version, name, motd)
     server.install_server()
@@ -13,10 +15,12 @@ def create_server(name, motd, software, version) -> Server:
     return server
 
 def load_server(name) -> Server:
-    if name in get_servers():
-        server_data = load_server_data()[name]
-        server = Server(server_data["software"], server_data["version"], server_data["disp_name"], server_data["motd"])
-        return server
+    servers = get_servers()
+    if servers:
+        if name in servers:
+            server_data = load_server_data()
+            server = Server(server_data["software"], server_data["version"], server_data["disp_name"], server_data["motd"])
+            return server
 
     raise ServerDoesNotExistError(f"Server '{name}' tried to load but was not found.")
 
